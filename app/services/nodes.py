ce2a0eee5c71fb448e955b12ba99a2822d738314
haram <dlfnadl97@naver.com>
2026-07-29 09:56:47 +0900

    hotfix:콤마추가

--- app/services/nodes.py
+++ app/services/nodes.py
@@ -157,6 +157,6 @@ async def reject_node(state: ReviewBotState, runtime: Runtime[ReviewBotContext])
         repo=state["repo"],
         pull_number=state["pull_number"],
         token=state["access_token"],
-        event="REQUEST_CHANGES"
+        event="REQUEST_CHANGES",
         body="리뷰를 거절합니다.",  # TODO : 좀더 명확하게 수정 ex. 거절 이유
     )
